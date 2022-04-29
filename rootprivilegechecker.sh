4a3b83ad76fa9af543c38d82b3e1917bf4f79239
Tamilarasu Alma Linux <Tamilarasu Alma Linux>
2022-04-29 22:49:58 +0530

    Basic Working Script

---
+++ rootprivilegechecker.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#Get the information of the groups of current user
+
+CURRENT_USER=$(whoami)
+GROUPS_LIST=$(getent group | grep "${CURRENT_USER}" | cut -d ":" -f 1)
+echo "${GROUPS_LIST}" > group.txt
+
+#Loop through the variable and do the thing
+
+while IFS="@" read -r group
+do
+    case "${group}" in
+    "wheel")
+        echo "You have superuser privileges !!!"
+    ;;
+
+    "sudo")
+        echo "You have superuser privileges !!!"
+    ;;
+
+    "root")
+        echo "You have superuser privileges !!!"
+    ;;
+
+    esac
+done < group.txt
